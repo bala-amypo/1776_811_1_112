49ea19353c4fb41f04c8b04bbe3f33e6a3b20553
bala-amypo <bala@amypo.in>
2025-12-27 03:49:36 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VerificationRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VerificationRequestServiceImpl.java
@@ -1,10 +1,7 @@
 package com.example.demo.service.impl;
-
 import java.time.LocalDate;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.AuditTrailRecord;
 import com.example.demo.entity.CredentialRecord;
 import com.example.demo.entity.VerificationRequest;
